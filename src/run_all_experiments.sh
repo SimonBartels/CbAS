5bff8b10de78d276d5f93e59dfd3fcf63a35facc
Simon Bartels <bartels@di.ku.dk>
2023-02-28 20:11:24 +0100

    ready to run large scale stuff

--- src/run_all_experiments.sh
+++ src/run_all_experiments.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #SBATCH --ntasks=1 --cpus-per-task=4 --mem=5000M
 # SBATCH --time=0-01:00:00
-sbatch run_cbas_fluorescence.sh --array=0-1
+sbatch  --array=0-4%1 run_cbas_fluorescence.sh
+sbatch  --array=0-4%1 run_cbas_vae.sh
---
+++ src/run_cbas_vae.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --ntasks=1 --cpus-per-task=4 --mem=5000M
+# SBATCH --time=0-01:00:00
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+#export PYTHONPATH  # why is this necessary?
+python run_cbas.py CBAS_VAE $SLURM_ARRAY_TASK_ID
