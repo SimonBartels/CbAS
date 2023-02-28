965ed559dd1dbdf1889b2dc3622d6c036b91e965
Simon Bartels <bartels@di.ku.dk>
2023-02-28 19:12:06 +0100

    specified resources

--- src/run_cbas_fluorescence.sh
+++ src/run_cbas_fluorescence.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#SBATCH --ntasks=1 --cpus-per-task=4 --mem=5000M
+# SBATCH --time=0-01:00:00
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 #export PYTHONPATH  # why is this necessary?
